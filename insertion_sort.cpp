a8fa28c9870b5c1e4dfa548801ec04fb8dfff5b7
Pratyush Pandey <64234448+Back-Log-Pratyush@users.noreply.github.com>
2020-05-28 22:13:49 +0530

    Insertion sort

---
+++ insertion_sort.cpp
@@ -0,0 +1,86 @@
+#include <algorithm>
+#include <bitset>
+#include <cassert>
+#include <chrono>
+#include <cmath>
+#include <complex>
+#include <cstdio>
+#include <cstdlib>
+#include <cstring>
+#include <ctime>
+#include <deque>
+#include <functional>
+#include <iomanip>
+#include <iostream>
+#include <iterator>
+#include <limits>
+#include <list>
+#include <map>
+#include <numeric>
+#include <queue>
+#include <random>
+#include <ratio>
+#include <set>
+#include <sstream>
+#include <stack>
+#include <string>
+#include <unordered_map>
+#include <unordered_set>
+#include <utility>
+#include <vector>
+using namespace std;
+#define ll long long
+#define MAX 1e9
+#define MIN -1e9
+#define max(x,y) (x>y)?x:y
+#define min(x,y) (x<y)?x:y
+#define s(n) scanf("%d",&n)
+#define mem memset(a, 0, sizeof(a))
+#define minimum(a)	*min_element(a.begin(), a.end())
+#define maximum(a)	*max_element(a.begin(), a.end())
+typedef vector<int> vi;
+typedef vector<string> vs;
+/*
+* * * * * * * * * * * * * * * * * * * * * *
+*       ☆꧁𝓑𝓐𝓒𝓚_𝓛𝓞𝓖꧂☆              **
+*                                        ***
+* * * * * * * * * * * * * * * * * * * * *****
+*/ 
+//Insertion sort==O(n^2)
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+     #ifndef ONLINE_JUDGE
+		  freopen("input.txt","r",stdin);
+	    freopen("output.txt","w",stdout);
+    #endif
+    int n;
+    cin>>n;
+    int arr[100];
+    for(int i=0;i<n;i++)
+    {
+        cin>>arr[i];
+    }
+    for(int i=1;i<=n-1;i++)
+    {
+        //we are chossing the currennt element in the array and we are
+        //puting this element to their right position
+        int e=arr[i];
+        int k=i-1;
+        while(k>=0 && arr[k]>e)
+        {
+            arr[k+1]=arr[k];
+            //copying the cuurent element to the kth index of the array
+            k--;
+        }
+  arr[k+1]=e;
+
+    }
+        
+        for(int i=0;i<n;i++)
+        {
+            cout<<arr[i]<<" ";
+        }
+return 0;
+}
