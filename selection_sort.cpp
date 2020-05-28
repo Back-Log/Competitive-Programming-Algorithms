8bdea0fffce098ba9978b74643e59e235e8bb8cf
Pratyush Pandey <64234448+Back-Log-Pratyush@users.noreply.github.com>
2020-05-28 22:14:44 +0530

    Selection Sort

---
+++ selection_sort.cpp
@@ -0,0 +1,87 @@
+#include <algorithm>
+#include <bitset>
+#include <cassert>
+#include <chrono>
+#include <cmath>
+#include <complex>
+#include <cstdio>
+#include <cstdlib>
+#include <cstring>
+#include <ctime>
+#include <deque>
+#include <functional>
+#include <iomanip>
+#include <iostream>
+#include <iterator>
+#include <limits>
+#include <list>
+#include <map>
+#include <numeric>
+#include <queue>
+#include <random>
+#include <ratio>
+#include <set>
+#include <sstream>
+#include <stack>
+#include <string>
+#include <unordered_map>
+#include <unordered_set>
+#include <utility>
+#include <vector>
+using namespace std;
+#define ll long long
+#define MAX 1e9
+#define MIN -1e9
+#define max(x,y) (x>y)?x:y
+#define min(x,y) (x<y)?x:y
+#define s(n) scanf("%d",&n)
+#define mem memset(a, 0, sizeof(a))
+#define minimum(a)	*min_element(a.begin(), a.end())
+#define maximum(a)	*max_element(a.begin(), a.end())
+typedef vector<int> vi;
+typedef vector<string> vs;
+/*
+* * * * * * * * * * * * * * * * * * * * * *
+*       ☆꧁𝓑𝓐𝓒𝓚_𝓛𝓞𝓖꧂☆              **
+*                                        ***
+* * * * * * * * * * * * * * * * * * * * *****
+*/ 
+//Selection sort algorithm
+//O(n^2)=time complexity
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+     #ifndef ONLINE_JUDGE
+		  freopen("input.txt","r",stdin);
+	    freopen("output.txt","w",stdout);
+    #endif
+    int n;
+    cin>>n;
+    int arr[100];
+    for(int i=0;i<n;i++)
+    {
+        cin>>arr[i];
+    }
+    for(int i=0;i<n;i++)
+     {
+         //we are checking the first min element in the array 
+         int check=MAX,index;
+         for(int j=i;j<n;j++)
+         {
+          if(arr[j]<check)
+          {
+              check=arr[j];
+              index=j;
+          }
+         }
+         swap(arr[i],arr[index]);
+         //hear we are sweping first min element to the first index and same proccess 
+         //for remaing element
+     }
+     for(int i=0;i<n;i++)
+     {
+         cout<<arr[i]<<" ";
+     }
+return 0;
+}
