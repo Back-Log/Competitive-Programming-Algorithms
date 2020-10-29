48e756b4ada769839a866ae86a49b6ac417bde37
Back-Log <pratyushnasa@gmail.com>
2020-10-29 09:02:06 +0530

    Dynamic Programming problems

---
+++ Dynamic Programming/coin_change.cpp
@@ -0,0 +1,28 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+  int coins[]={1,2,5,10,20,50,100,200,500,2000};
+  //number of coin you want to mak a change
+  int n;
+  cin>>n;
+  int dp[10000]={0};
+  int mi=INT_MAX;
+  for(int i=1;i<=n;i++)
+  {
+  	mi=INT_MAX;
+  	for(auto c:coins)
+  	{
+  		if(i-c>=0)
+  		{
+          mi=min(mi,dp[i-c]);
+  		}
+  	}
+  	dp[i]=mi+1;
+  }
+ 
+ cout<<dp[n]<<endl; 
+
+return 0;
+
+}
